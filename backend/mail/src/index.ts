77c02064c7e5a15ec9a21507807676227ac5fca6
RuchirSharmaIITG <ruchirsharma949@gmail.com>
2026-06-03 01:22:27 +0530

    Initialize mail microservice and background consumer

---
+++ backend/mail/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { startSendOtpConsumer } from "./consumer.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+startSendOtpConsumer();
+
+const app = express();
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
