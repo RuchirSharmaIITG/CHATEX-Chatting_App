2d64bda25e5f34a0a61abf3d8f1a93f7dce47d5d
RuchirSharmaIITG <ruchirsharma949@gmail.com>
2026-06-04 01:58:04 +0530

    Update mail

--- backend/mail/src/consumer.ts
+++ backend/mail/src/consumer.ts
@@ -1,10 +1,10 @@
-import amqp from 'amqplib';
-import nodemailer from 'nodemailer';
+import amqp from "amqplib";
+import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const startSendOtpConsumer = async() => {
-  try{
+export const startSendOtpConsumer = async () => {
+  try {
     const connection = await amqp.connect({
       protocol: "amqp",
       hostname: process.env.Rabbitmq_Host!,
@@ -13,23 +13,23 @@ export const startSendOtpConsumer = async() => {
       password: process.env.Rabbitmq_Password!,
     });
 
-    const channel = await connection.createChannel()
+    const channel = await connection.createChannel();
 
-    const queueName = "send-otp"
+    const queueName = "send-otp";
 
-    await channel.assertQueue(queueName, { durable: true});
+    await channel.assertQueue(queueName, { durable: true });
 
     console.log("✅ Mail Service consumer started, listening for otp emails");
 
-    channel.consume(queueName, async(msg)=>{
-      if(msg){
+    channel.consume(queueName, async (msg) => {
+      if (msg) {
         try {
-          const {to, subject, body} = JSON.parse(msg.content.toString())
+          const { to, subject, body } = JSON.parse(msg.content.toString());
 
           const transporter = nodemailer.createTransport({
-            host:"smtp.gmail.com",
+            host: "smtp.gmail.com",
             port: 465,
-            auth:{
+            auth: {
               user: process.env.USER,
               pass: process.env.PASSWORD,
             },
@@ -48,8 +48,8 @@ export const startSendOtpConsumer = async() => {
           console.log("Failed to send otp", error);
         }
       }
-    })
+    });
   } catch (error) {
     console.log("Failed to start rabbitmq consumer", error);
   }
-}
+};
--- backend/mail/src/index.ts
+++ backend/mail/src/index.ts
@@ -1,13 +1,13 @@
 import express from "express";
-import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 import { startSendOtpConsumer } from "./consumer.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 startSendOtpConsumer();
 
